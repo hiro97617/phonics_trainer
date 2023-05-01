136dc0631cc72e9d69d70a2f82dabd1141d9c590
hiro97617 <cf24bnva5@gmail.com>
2023-05-01 14:09:34 +0900

    add header and footer

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -8,9 +8,6 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root to: 'dashboards#index'
-    get 'login', to: 'user_sessions#new'
-    post 'login', to: 'user_sessions#create'
-    delete 'logout', to: 'user_sessions#destroy'
     resources :level_parts
   end
 end
