9412d25f63a385ac5925f9bb638adbb0188a640a
hiro97617 <cf24bnva5@gmail.com>
2023-05-23 15:56:21 +0900

    first commit

--- src/app/models/tag.rb
+++ src/app/models/tag.rb
@@ -1,2 +1,4 @@
 class Tag < ApplicationRecord
+  has_many :multiple_choice_question_tags
+  has_many :multiple_choice_questions, through: :multiple_choice_question_tags
 end
--- src/config/application.rb
+++ src/config/application.rb
@@ -20,6 +20,7 @@ module App
     config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}').to_s]
     config.i18n.available_locales = %i[ja en]
     config.i18n.default_locale = :ja
+    config.autoload_paths += Dir.glob("#{config.root}/app/forms")
 
     # config.eager_load_paths << Rails.root.join("extras")
   end
