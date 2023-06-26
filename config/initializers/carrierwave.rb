1ee41b8a150b0f747e7aea0d49552df9e646400a
hiro97617 <cf24bnva5@gmail.com>
2023-06-26 14:33:26 +0900

    third commit

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,17 +6,15 @@ CarrierWave.configure do |config|
   if Rails.env.production? # 本番環境の場合はS3へアップロード
     config.storage :fog
     config.fog_provider = 'fog/aws'
-    config.fog_directory  = 'phonics-trainer' # バケット名
+    config.fog_directory  = ENV['BUCKET_NAME'] # バケット名
     config.fog_public = false
     config.fog_credentials = {
       provider: 'AWS',
-      access_key_id: Rails.application.credentials.dig(:aws, :access_key_id), # アクセスキー
-      secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key), # シークレットアクセスキー
+      access_key_id: ENV['ACCESS_KEY_ID'], # アクセスキー
+      secret_access_key: ENV['SECRET_ACCESS_KEY'], # シークレットアクセスキー
       region: 'ap-northeast-1', # リージョン
       path_style: true
     }
-    config.fog_directory = 'phonics-trainer'
-    config.asset_host = "https://s3-ap-northeast-1.amazonaws.com/phonics-trainer"
   else # 本番環境以外の場合はアプリケーション内にアップロード
     config.storage :file
     config.enable_processing = false if Rails.env.test?
