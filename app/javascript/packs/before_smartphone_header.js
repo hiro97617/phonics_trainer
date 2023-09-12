75afb39b11a45268442c955445afdf51bca5e51a
hiro97617 <cf24bnva5@gmail.com>
2023-09-12 19:09:14 +0900

    first commit

---
+++ app/javascript/packs/before_smartphone_header.js
@@ -0,0 +1,13 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+  if (document.getElementById('nav-toggle')) {
+    document.getElementById('after-login-nav-toggle').addEventListener('click', function() {
+    let element = document.getElementById("beforeToggle");
+    if (element.style.display === 'none') {
+      element.style.display = 'block';
+    } else {
+      element.style.display = 'none';
+    }
+      })
+      }
+})
